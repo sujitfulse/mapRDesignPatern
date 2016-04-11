5c121e86dfb04ff0324f5540b1991bff75df9c86
sujit <sujitfulse@gmail.com>
2016-04-11 17:14:07 +0530

    TomWhite-Book

---
+++ Ch2Summariztion/src/numerical/medianstddev/MRDPUtils.java
@@ -0,0 +1,33 @@
+package numerical.medianstddev;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MRDPUtils {
+
+  public static final String[] REDIS_INSTANCES =
+      { "p0", "p1", "p2", "p3", "p4", "p6" };
+
+  // This helper function parses the stackoverflow into a Map for us.
+  public static Map<String, String> transformXmlToMap(String xml) {
+    Map<String, String> map = new HashMap<String, String>();
+    try {
+      String[] tokens =
+          xml.trim().substring(5, xml.trim().length() - 3).split("\"");
+    //  System.out.println("xml : "+xml);
+    //  System.out.println("xml.trim().length() "+ xml.trim().length()); cut first 5 to last-3
+      
+      for (int i = 0; i < tokens.length - 1; i += 2) {
+        String key = tokens[i].trim();
+        String val = tokens[i + 1];
+        map.put(key.substring(0, key.length() - 1), val);
+      //  System.out.println("*********Key : "+key + " **Val"+val);
+      //  System.out.println("----------------------------------------------------");
+      }
+    } catch (StringIndexOutOfBoundsException e) {
+      System.err.println(xml);
+    }
+
+    return map;
+  }
+}
---
+++ Ch6MetaPattern/src/MRDPUtils.java
@@ -0,0 +1,52 @@
+
+
+
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+public class MRDPUtils {
+	private static final Pattern INTEGER = Pattern.compile("\\d+");
+
+	public static Map<String, String> transformXmlToMap(String xml) {
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			String[] tokens = xml.trim().substring(5, xml.trim().length() - 3).split("\"");
+			for (int i = 0; i < tokens.length; i += 2) {
+				String key = tokens[i].trim();
+				String val;
+				if (i + 1 >= tokens.length) {
+					val = null;
+				} else {
+					val = StringEscapeUtils.unescapeHtml(tokens[i + 1].trim());
+				}
+
+				map.put(key.substring(0, key.length() - 1), val);
+			}
+		} catch (StringIndexOutOfBoundsException e) {
+			System.err.println(xml);
+		}
+
+		return map;
+	}
+
+	public final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss.sss");
+
+	public static boolean isNullOrEmpty(String str) {
+		return str == null || str.length() == 0;
+	}
+
+	public static boolean isInteger(String str) {
+		if (str == null) {
+			return false;
+		}
+		Matcher is = INTEGER.matcher(str);
+		return is.matches();
+	}
+}
